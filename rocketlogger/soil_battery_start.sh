802103af1c7477362a841ea1369fc2630f920b4b
Neal Jackson <nealjack@umich.edu>
2020-02-20 15:20:14 -0800

    add rocketlogger scripts/service

---
+++ rocketlogger/soil_battery_start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"`
+NUMBER=`cat /home/rocketlogger/num.txt`
+FNAME=soil_${DATE_WITH_TIME}_${NUMBER}.csv
+
+echo $FNAME
+
+rocketlogger stop
+sleep 1s
+rocketlogger cont -r 1 -f /home/rocketlogger/soil_battery/$FNAME -format csv -ch 0,1,2 -size 1g -d 0 -w 0
+echo $(( $NUMBER+1 )) > /home/rocketlogger/num.txt
---
+++ rocketlogger/soil_battery_stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rocketlogger stop
+sleep 1s
