ad64bdfdaedf040a864549b03fb677e66cd39e06
Demo User <rocketlogger@beaglebone.localdomain>
2020-11-11 02:03:40 +0000

    update script to log I2 and V2

--- rocketlogger/soil_battery_start.sh
+++ rocketlogger/soil_battery_start.sh
@@ -8,7 +8,7 @@ echo $FNAME
 
 rocketlogger stop
 sleep 1s
-rocketlogger cont -r 1 -f /home/rocketlogger/soil_battery/$FNAME -format csv -ch 0,1,2 -size 1g -d 0 -w 0
+rocketlogger cont -r 1 -f /home/rocketlogger/soil_battery/$FNAME -format csv -ch 0,1,2,3,4,5 -size 1g -d 0 -w 0
 echo $(( $NUMBER+1 )) > /home/rocketlogger/num.txt
 
 teroslogger -t /dev/ttyACM0 -n $NUMBER
