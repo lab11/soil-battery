f11aa8b23ab185a730e08ecfa24dbd19ee3fa7b6
timsliu <timliu92@gmail.com>
2020-12-21 16:16:37 -0800

    Changed ch 3 to 6 in soil_battery_start.sh
    rocketlogger 2 failing to send data; rocketlogger 2 replaced with rocketlogger 1 (previously unused)

--- rocketlogger/soil_battery_start.sh
+++ rocketlogger/soil_battery_start.sh
@@ -9,7 +9,7 @@ echo $FNAME
 
 rocketlogger stop
 sleep 1s
-rocketlogger cont -r 1 -f /home/rocketlogger/soil_battery/$FNAME -format csv -ch 0,1,2,3,4,5 -size 1g -d 0 -w 0
+rocketlogger cont -r 1 -f /home/rocketlogger/soil_battery/$FNAME -format csv -ch 0,1,2,6,4,5 -size 1g -d 0 -w 0
 echo $(( $NUMBER+1 )) > /home/rocketlogger/num.txt
 
 if test -e "$DEV"; then
